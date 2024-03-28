2093e4d3176e71cc6b26b73e9314f3735c411714
George <ndungugeorge910@gmail.com>
2024-03-28 09:17:48 -0400

    nodes with child node

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,7 +11,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree)
 	{
 		if (tree->left || tree->right)
-			node++;
+			nodes++;
 		nodes += binary_tree_nodes(tree->left);
 		nodes += binary_tree_nodes(tree->right);
 	}
